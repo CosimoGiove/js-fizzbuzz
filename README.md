5168bf5b222a65a92bbf8649539f7755f200e1b3
cosimogiove <cosimo18497@gmail.com>
2023-11-08 16:55:08 +0100

    feat:fine

--- README.md
+++ README.md
@@ -4,10 +4,7 @@ esercizio:
 Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 ## esecuzione scritta
-
-1. creare una costante  numero 0
-2. creare una costante  numero 100
-3. creare un ciclo for , dove incremento di 1 fino al raggiungimento di 99
+3. creare un ciclo for , dove incremento di 1 fino al raggiungimento di 100
 4. all'interno del ciclo for  utilizzo le condizioni if else per indicare i multipli 3 e di 5
 5. per i numeri multipli di 3 stampare in console fizz 
 6. per i multipli di 5 stampare in console buzz
---
+++ main.js
@@ -0,0 +1,37 @@
+"use strict"
+// lista dell ul  che deve contenere li//
+const ListaBuzzFizz = document.getElementById("lista");
+
+//ciclo for da 1 a 101//
+for (let conteggio = 1; conteggio <= 100; conteggio++) {
+    //creazione elemento li//
+    let ELemento = document.createElement("li")
+     
+    if (conteggio % 3 === 0 && conteggio % 5 === 0) {
+        console.log("FizzBuzz");
+        ELemento.append("FizzBuzz");
+        ListaBuzzFizz.append(ELemento);
+        ELemento.classList.add("color")
+    } else if (conteggio % 3 === 0) {
+        console.log("Fizz");
+        ELemento.append("Fizz");
+        ListaBuzzFizz.append(ELemento);
+        ELemento.classList.add("green")
+    } else if (conteggio % 5 === 0) {
+        console.log("Buzz");
+        ELemento.append("Buzz");
+        ListaBuzzFizz.append(ELemento);
+        ELemento.classList.add("yellow")
+    } else {
+        console.log(conteggio);
+        ELemento.append(conteggio);
+        ListaBuzzFizz.append(ELemento);
+        ELemento.classList.add("blue")
+    }
+}
+
+
+
+
+
+
